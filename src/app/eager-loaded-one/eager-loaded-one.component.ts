496f05409c2b45b6d1460defb8f74ab76096a332
fullstackdeveloperbilbao <fullstackdeveloperbilbao@gmail.com>
2022-06-20 13:33:36 +0100

    done

--- src/app/eager-loaded-one/eager-loaded-one.component.ts
+++ src/app/eager-loaded-one/eager-loaded-one.component.ts
@@ -4,7 +4,8 @@ import { SharedService } from '../shared/shared.service';
 @Component({
   selector: 'app-eager-loaded-one',
   templateUrl: './eager-loaded-one.component.html',
-  styleUrls: ['./eager-loaded-one.component.scss']
+  styleUrls: ['./eager-loaded-one.component.scss'],
+  providers: [SharedService]
 })
 export class EagerLoadedOneComponent implements OnInit {
 
--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 
 console.log("%cSharedService" + "%c bundled", "color:red", "color:green");
 @Injectable({
   providedIn: 'any'
 })
-export class SharedService {
+export class SharedService implements OnDestroy {
 
   constructor() {
     console.log("%cSharedService" + "%c instantiated", "color:red", "color:green");
+  }
+
+  ngOnDestroy(): void {
+    console.log("%cSharedService" + "%c destroyed", "color:red", "color:green");
   }
 }
